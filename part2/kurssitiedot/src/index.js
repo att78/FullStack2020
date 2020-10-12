4f80e8ca0d7cb588fbe72680b2bd73cf44e78f97
Auli Tallberg <auli.tallberg@gmail.com>
2020-10-12 11:12:20 +0300

    2.4 done

--- part2/kurssitiedot/src/index.js
+++ part2/kurssitiedot/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // Header komponenttiin otetaan vain alkuperäisen taulukon name-tunniste sisään.
 const Header = ({ name }) => {
   return (
-    <h1>{name}</h1>
+    <h2>{name}</h2>
   )
 }
 
@@ -36,7 +36,7 @@ function Total({ parts }) {
   }, 0);
 
   return (
-    <p>Number of exercises {sum}</p>
+    <p>Total of exercises is {sum}</p>
   );
 }
 
@@ -58,31 +58,56 @@ const Course = ({ course }) => {
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-  console.log(course)
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  //console.log(course)
   return (
     <div>
-      <Course course={course} />
+      <h1>List of course content</h1>
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
   )
 }
