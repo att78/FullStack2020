fa5292ea6464900e74916dd75904712df8ca038c
Auli Tallberg <auli.tallberg@gmail.com>
2020-12-08 13:07:53 +0200

    4.4 and 4.5 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // sum asetetaan alussa nollaksi. blog arvo kuvaa kunkin yksittäisen blogin likejen määrää.
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const mostLikedBlog = (blogs) => {
+
+  return blogs.reduce((fBlog, blog) => fBlog.likes > blog.likes ? fBlog : blog, {})
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  mostLikedBlog
 }
